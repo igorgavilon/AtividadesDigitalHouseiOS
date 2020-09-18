d011346dbaa33721b240b42216bfc0c4635d3c9d
Igor Gavilon <igorgavilon@gmail.com>
2020-09-17 22:10:44 -0400

    App Lista de compras : implementadas todas as funcionalidades

--- App_Lista_Compras/ListaCompras/ListaCompras/ViewController.swift
+++ App_Lista_Compras/ListaCompras/ListaCompras/ViewController.swift
@@ -15,11 +15,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var textFieldQuantidade: UITextField!
     @IBOutlet weak var labelListagem: UILabel!
     @IBOutlet weak var labelStatus: UILabel!
+    @IBOutlet weak var labelNomeError: UILabel!
+    @IBOutlet weak var labelQtdError: UILabel!
+    @IBOutlet weak var buttonSalvar: UIButton!
+    @IBOutlet weak var buttonLimpar: UIButton!
+    @IBOutlet weak var buttonExcluir: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         textFieldNome.delegate = self
+        textFieldQuantidade.delegate = self
+        labelStatus.isHidden = true
+        labelNomeError.isHidden = true
+        labelQtdError.isHidden = true
+        buttonExcluir.isHidden = true
+        labelListagem.text = ""
+
     }
 
     
@@ -27,19 +39,27 @@ class ViewController: UIViewController {
         let nomeItem = textFieldNome.text!
         let qtdItem = textFieldQuantidade.text!
         
-        //verificar se item já existe
-        if let item = buscarItem(nome: nomeItem){
-            //se item existe, editar
-            labelStatus.text = "Editar"
-            editarItem(item: item)
-        }else{
-            //cadastrar novo item
-            labelStatus.text = "Produto Novo"
+        if validaNome() && validaQuantidade(){
+            //verificar se item já existe
+            if let item = buscarItem(nome: nomeItem){
+                //se item existe, editar
+                labelStatus.text = "Produto Editado."
+                editarItem(item: item)
+                buttonExcluir.isHidden = true
 
-            cadastrarItem(nome: nomeItem, quantidade: qtdItem)
+            }else{
+                //cadastrar novo item
+                labelStatus.text = "Produto Novo Cadastrado."
+
+                cadastrarItem(nome: nomeItem, quantidade: qtdItem)
+                
+            }
+            //atualiza o label listagem
+            atualizaListagem()
+            //limpar campos
             limparCampos()
-            //inserir no label listagem
-            labelListagem.text = nomeItem
+
+            
         }
         
     }
@@ -51,31 +71,29 @@ class ViewController: UIViewController {
     
     @IBAction func buttonExcluirClick(_ sender: Any) {
         let nomeItem = textFieldNome.text!
-        let qtdItem = textFieldQuantidade.text!
         
-        //verificar se item já existe
-        if let item = buscarItem(nome: nomeItem){
-            //se item existe, editar
-            labelStatus.text = "Item Removido."
-            excluirItem(item: item)
-            //implementar a retirada do item do label listagem
-        }else{
-            //cadastrar novo item
-            labelStatus.text = "Produto Novo"
+        if validaNome(){
+            //verificar se item já existe
+            if let item = buscarItem(nome: nomeItem){
+                //se item existe, editar
+                labelStatus.text = "Item Removido."
+                excluirItem(item: item)
+                buttonExcluir.isHidden = true
 
-            cadastrarItem(nome: nomeItem, quantidade: qtdItem)
+            }
+            //atualizar o label listagem
+            atualizaListagem()
+            //limpar campos
             limparCampos()
-            //inserir no label listagem
-            labelListagem.text = nomeItem
         }
     }
     
-    func cadastrarItem(nome: String,  quantidade: String){
+    private func cadastrarItem(nome: String,  quantidade: String){
         let item = Item(nome: nome, quantidade: Int(quantidade)!)
         arrayItens.append(item)
     }
 
-    func editarItem(item: Item){
+    private func editarItem(item: Item){
         for itemArray in arrayItens{
             if itemArray.getNome() == item.getNome(){
                 itemArray.setQuantidade(quantidade: item.getQuantidade())
@@ -84,7 +102,7 @@ class ViewController: UIViewController {
         
     }
     
-    func excluirItem(item: Item){
+    private func excluirItem(item: Item){
         for (index, itemArray) in arrayItens.enumerated(){
             if itemArray.getNome() == item.getNome(){
                 arrayItens.remove(at: index)
@@ -93,9 +111,13 @@ class ViewController: UIViewController {
         }
     }
     
-    func buscarItem(nome: String) -> Item?{
+    private func buscarItem(nome: String) -> Item?{
+        let nomeLowerCase = nome.folding(options: .diacriticInsensitive, locale: .current).lowercased()
+
         for item in arrayItens{
-            if item.getNome() == nome{
+            let itemNome = item.getNome()
+            let itemNomeLowerCase = itemNome.folding(options: .diacriticInsensitive, locale: .current).lowercased()
+            if  itemNomeLowerCase == nomeLowerCase{
                 return item
             }
         }
@@ -106,20 +128,86 @@ class ViewController: UIViewController {
         textFieldNome.text = ""
         textFieldQuantidade.text = ""
     }
+    
+    private func validaNome() -> Bool{
+        labelNomeError.isHidden = true
+        if textFieldNome.text == nil || textFieldNome.text!.isEmpty{
+            labelNomeError.isHidden = false
+            return false
+        }
+        return true
+    }
+    
+    private func validaQuantidade() -> Bool{
+        labelQtdError.isHidden = true
+        if textFieldQuantidade.text == nil || textFieldQuantidade.text!.isEmpty{
+            labelQtdError.isHidden = false
+            return false
+        }
+        return true
+    }
+    
+    private func atualizaListagem(){
+        let novaLinha = "\n"
+        labelListagem.text = ""
+        for item in arrayItens{
+            labelListagem.text = "\(labelListagem.text!)\(item.getNome()) \(novaLinha)"
+        }
+    }
+    
 }
 
 extension ViewController: UITextFieldDelegate{
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == textFieldNome{
-            if let item = buscarItem(nome: textFieldNome.text!){
-                //se existir o item, popular os textField
-                labelStatus.text = "Produto já existe."
-                textFieldQuantidade.text = String(item.getQuantidade())
+            if validaNome(){
+                if let item = buscarItem(nome: textFieldNome.text!){
+                    //se existir o item, popular os textField
+                    labelStatus.text = "Produto já existente."
+                    textFieldQuantidade.text = String(item.getQuantidade())
+                    buttonExcluir.isHidden = false
+                }else{
+                    labelStatus.text = "Produto Novo."
+                    
+                }
+                labelStatus.isHidden = false
                 textFieldQuantidade.becomeFirstResponder()
+            }
+        }else{
+            if !validaNome(){
+                textFieldNome.becomeFirstResponder()
+            }
+            else if validaQuantidade(){
+                textField.resignFirstResponder()
             }
             
         }
-        
         return true
+        
+        
     }
+    /*
+     
+    if textField == textFieldNome{
+        if let item = buscarItem(nome: textFieldNome.text!){
+            //se existir o item, popular os textField
+            labelStatus.text = "Produto já existente."
+            textFieldQuantidade.text = String(item.getQuantidade())
+        }else{
+            labelStatus.text = "Produto Novo."
+
+        }
+        textFieldQuantidade.becomeFirstResponder()
+
+    }else{
+        
+    }
+    
+    return true
+     
+     */
+    
+    
+    
+    
 }
